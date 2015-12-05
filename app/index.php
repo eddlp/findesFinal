fc1f1f42fc3312139c61848f97855a6efacc6008
Tebi <tebii.11@hotmail.com>
2015-12-05 12:08:17 -0300

    Links to css and scripts created. Index updated.

--- app/index.php
+++ app/index.php
@@ -3,12 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <title></title>
+    <?php
+    require'view/sections/links.html';
+    ?>
 </head>
 <body>
 
 <?php
+require'view/sections/header.php';
+require'view/modules/index.php';
+require'view/sections/footer.php';
 
-
+require'view/sections/scripts.html';
 ?>
 
 </body>
