edf91492d54aff046357a64fd9a7e78887b7a6dd
Tebi <tebii.11@hotmail.com>
2015-12-05 15:19:57 -0300

    Estado views updated.

---
+++ app/est_new.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <?php
+    require'view/sections/links.html';
+    ?>
+</head>
+<body>
+
+<?php
+require'view/sections/header.php';
+require'view/modules/estado/est_new.php';
+require'view/sections/footer.php';
+
+require'view/sections/scripts.html';
+?>
+
+</body>
+</html>
