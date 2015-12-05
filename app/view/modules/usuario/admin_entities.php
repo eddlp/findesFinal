d69f65e30ea15a00db034b4b21f9faff005ba20f
Tebi <tebii.11@hotmail.com>
2015-12-05 12:31:07 -0300

    AdministrarEntidades father and view created.

---
+++ app/view/modules/usuario/admin_entities.php
@@ -0,0 +1,18 @@
+<div class="container">
+    <div class="row">
+        <div class="row">
+            <h2>Administrador de Entidades</h2>
+            <div class="col-md-6 col-xs-12">
+                Texto descripcion
+            </div>
+            <div class="col-md-6 col-xs-12">
+                <ul>
+                    <li>
+                        <a href="est_list.php">Estado</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+</div>
