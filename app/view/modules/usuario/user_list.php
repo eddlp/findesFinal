705d2f194d718fddda950c7d90b3ce43580415e1
AdrianTrik <Adrian.trik@hotmail.com>
2015-12-06 21:30:45 -0300

    Listado de usuario - Administrador

--- app/view/modules/usuario/user_list.php
+++ app/view/modules/usuario/user_list.php
@@ -1,3 +1,9 @@
+<?php
+    use app\repository\UsuarioRepository;
+    require_once('repository/UsuarioRepository.php');
+    require_once('repository/Connection.php');
+    require_once('model/Usuario.php');
+?>
 <div class="container">
     <div class="row">
         <h3>Listado de Usuarios</h3>
@@ -9,21 +15,30 @@
                     <th>Email</th>
                     <th>Habilitado</th>
                 </tr>
-
+                <?php
+                    $usuarioRepository = new UsuarioRepository();
+                    $usuarios = $usuarioRepository->getAll();
+                    foreach ($usuarios as $u) {
+                ?>
                 <tr>
                     <td>
-
+                        <?php echo($u->getId());?>
                     </td>
                     <td>
-
+                        <?php echo($u->getUsername());?>
                     </td>
                     <td>
-
+                        <?php echo($u->getEmail());?>
                     </td>
                     <td>
-
+                        <input type="checkbox" disabled
+                               <?php if($u->getHabilitado()) {
+                                        echo('checked>');
+                                    }
+                               ?>
                     </td>
                 </tr>
+                <?php } ?>
             </table>
         </div>
     </div>
