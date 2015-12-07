144aa99941af12428fc62f35bbae4fe00516de4b
Tebi <tebii.11@hotmail.com>
2015-12-07 01:50:21 -0300

    Working with index view style.

--- app/view/modules/index.php
+++ app/view/modules/index.php
@@ -38,35 +38,56 @@
         <span class="sr-only">Next</span>
     </a>
 </div>
-<div class="container">
+<div class="container containerindex">
+    <div class="row bienvenida">
+        <div class="col-md-5 izq">
+            <h1 class="findes">Findes</h1>
+            <hr>
+            <p class="slogan">Donde vas a encontrar tu casa de fin de semana</p>
+        </div>
+        <div class="col-md-7 der">
+            <h1>Bienvenido a Findes</h1>
+            <p>Gracias por visitarnos, en este fantastico sitio web vas a poder encontrar la casa perfecta para vos y pasar un gran momento de descanso.</p>
+            <p>Solo queremos darte una calida bienvenida, no te robamos mas tiempo, encontra tu casa ahora!</p>
+        </div>
+    </div>
     <div class="row">
         <div class="col-sm-6 col-md-4">
             <div class="thumbnail">
-                <img src="..." alt="...">
+                <img src="view/images/casa/casa.jpg" alt="...">
                 <div class="caption">
-                    <h3>Thumbnail label</h3>
-                    <p>...</p>
-                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                    <h3>Casa Destacada 1</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed lorem at quam malesuada luctus.
+                        Integer malesuada est orci, at sollicitudin erat aliquam sed.
+                        Nullam nisl nisl, convallis ac nunc ut,
+                        pulvinar suscipit libero. Pellentesque placerat ante in justo viverra, vel rhoncus lectus fringilla.</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Ver detalle</a></p>
                 </div>
             </div>
         </div>
         <div class="col-sm-6 col-md-4">
             <div class="thumbnail">
-                <img src="..." alt="...">
+                <img src="view/images/casa/casa.jpg" alt="...">
                 <div class="caption">
-                    <h3>Thumbnail label</h3>
-                    <p>...</p>
-                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                    <h3>Casa Destacada 2</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed lorem at quam malesuada luctus.
+                        Integer malesuada est orci, at sollicitudin erat aliquam sed.
+                        Nullam nisl nisl, convallis ac nunc ut,
+                        pulvinar suscipit libero. Pellentesque placerat ante in justo viverra, vel rhoncus lectus fringilla.</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Ver detalle</a></p>
                 </div>
             </div>
         </div>
         <div class="col-sm-6 col-md-4">
             <div class="thumbnail">
-                <img src="..." alt="...">
+                <img src="view/images/casa/casa.jpg" alt="...">
                 <div class="caption">
-                    <h3>Thumbnail label</h3>
-                    <p>...</p>
-                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                    <h3>Casa Destada 3</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed lorem at quam malesuada luctus.
+                        Integer malesuada est orci, at sollicitudin erat aliquam sed.
+                        Nullam nisl nisl, convallis ac nunc ut,
+                        pulvinar suscipit libero. Pellentesque placerat ante in justo viverra, vel rhoncus lectus fringilla.</p>
+                    <p><a href="#" class="btn btn-primary" role="button">Ver detalle</a></p>
                 </div>
             </div>
         </div>
