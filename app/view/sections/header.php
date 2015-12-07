cf27719c30e91ac5a16c1f97c7ca491056abae0f
Tebi <tebii.11@hotmail.com>
2015-12-07 00:15:03 -0300

    Working with web style.

--- app/view/sections/header.php
+++ app/view/sections/header.php
@@ -6,8 +6,8 @@
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="index.php">Home  </a></li>
+            <ul class="nav navbar-nav izquierda">
+                <li class="activo"><a href="index.php">Home  </a></li>
                 <li><a href="about.php">Quienes somos</a></li>
                 <li><a href="catalogo.php">ENCONTRA TU CASA AHORA!</a></li>
             </ul>
