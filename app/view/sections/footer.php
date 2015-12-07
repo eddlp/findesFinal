7ec9b5c2699df34dd99b1fdd9f6261fa9bfeea53
Tebi <tebii.11@hotmail.com>
2015-12-07 02:12:37 -0300

    Footer finished.

--- app/view/sections/footer.php
+++ app/view/sections/footer.php
@@ -1,14 +1,27 @@
 <div class="row footer">
     <div class="container col-fot">
-        <div class="row">
-            <div class="col-md-4 col-xs-12  ">
-                <p>Texto primer columna</p>
+        <div class="row detalles">
+            <div class="col-md-4 col-xs-12 columna ">
+                <h2>Datos de contacto con la empresa</h2>
+                <h3>Ciudad</h3>
+                <p>Rosario, Prov. Santa Fe</p>
+                <h3>Telefonos</h3>
+                <p>(0341)4548793 / 4664133</p>
+                <h3>Correo electronico</h3>
+                <p>contacto@findes.com.ar</p>
             </div>
-            <div class="col-md-4 col-xs-12 ">
-                <p>Texto primer columna</p>
+            <div class="col-md-4 col-xs-12 columna ">
+                <h2>El sitio web</h2>
+                <a href="">Mapa del sitio</a><br>
+                <a href="">Encontra la casa que buscas</a><br>
+                <a href="">Registrate ahora.</a><br>
             </div>
-            <div class="col-md-4 col-xs-12  ">
-                <p>Texto primer columna</p>
+            <div class="col-md-4 col-xs-12 columna ">
+                <h2>Sistema de Privacidad</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed lorem at quam malesuada luctus.
+                    Integer malesuada est orci, at sollicitudin erat aliquam sed.
+                    Nullam nisl nisl, convallis ac nunc ut,
+                    pulvinar suscipit libero. Pellentesque placerat ante in justo viverra, vel rhoncus lectus fringilla.</p>
             </div>
         </div>
     </div>
